5cf26e04d3a29a3452243cf76547ec2200af15ce
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-01-11 17:44:41 +0530

    jquery

---
+++ Jquery-tutorial/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+  $('button').click(function()
+  {
+    document.body.style.backgroundColor="red"
+  })
+})
+
+// Events in Jquery
+//Mouse Events=click,dblclick,mouseenter,mouseleave
+//keyboardEvent=Keypress,Keydown,MediaKeystatusMap
+//Form Events=submit,change,focus,blur
+//document/window events=load,resize,scroll,unload.
